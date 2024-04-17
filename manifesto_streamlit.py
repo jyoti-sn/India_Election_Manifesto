14f4112dfbed4ee0925bb6a1578765a50a89a246
Jyoti S Nayak <125571567+jyoti-sn@users.noreply.github.com>
2024-04-17 11:48:04 +0530

    Update manifesto_streamlit.py

--- manifesto_streamlit.py
+++ manifesto_streamlit.py
@@ -15,10 +15,13 @@ inc_df = pd.read_csv(url_inc)
 st.title("India Election Manifesto Dashboard: 2004-2024")
 
 # Sidebar for selecting the years and compare option
-years = st.sidebar.slider("Select years", min_value=2004, max_value=2024, value=(2004, 2009, 2014, 2019, 2024), step=5)
+year_min = 2004
+year_max = 2024
+year_step = 5
+years = st.sidebar.slider("Select years", min_value=year_min, max_value=year_max, value=(year_min, year_max), step=year_step)
 all_years = st.sidebar.checkbox("Show data for all years")
 if all_years:
-    years = (2004, 2009, 2014, 2019, 2024)
+    years = (year_min, year_max)
 
 compare_parties = st.sidebar.checkbox("Compare BJP and INC")
 
@@ -29,11 +32,11 @@ custom_stop_words = ['Bharatiya', 'Janata', 'Party']
 if compare_parties:
     # Display the most common topics as a radar chart
     st.subheader("Most Common Topics")
-    bjp_topics = [x.strip() for topic in bjp_df[bjp_df['Year'].isin(years)]['Domains'].tolist() for x in topic.split(',')]
+    bjp_topics = [x.strip() for topic in bjp_df[bjp_df['Year'].between(years[0], years[1])]['Domains'].tolist() for x in topic.split(',')]
     bjp_topic_counts = pd.Series(bjp_topics).value_counts()
     bjp_topic_counts = bjp_topic_counts.reindex(bjp_topic_counts.nlargest(10).index)
 
-    inc_topics = [x.strip() for topic in inc_df[inc_df['Year'].isin(years)]['Domains'].tolist() for x in topic.split(',')]
+    inc_topics = [x.strip() for topic in inc_df[inc_df['Year'].between(years[0], years[1])]['Domains'].tolist() for x in topic.split(',')]
     inc_topic_counts = pd.Series(inc_topics).value_counts()
     inc_topic_counts = inc_topic_counts.reindex(inc_topic_counts.nlargest(10).index)
 
@@ -64,11 +67,11 @@ if compare_parties:
 
     # Display the most common subcategories
     st.subheader("Most Common Subcategories")
-    bjp_subcategories = [x.strip() for subcategory in bjp_df[bjp_df['Year'].isin(years)]['Topic_Subcategories'].tolist() for x in subcategory.split(',')]
+    bjp_subcategories = [x.strip() for subcategory in bjp_df[bjp_df['Year'].between(years[0], years[1])]['Topic_Subcategories'].tolist() for x in subcategory.split(',')]
     bjp_subcategory_counts = pd.Series(bjp_subcategories).value_counts()
     bjp_subcategory_counts = bjp_subcategory_counts.reindex(bjp_subcategory_counts.nlargest(10).index)
 
-    inc_subcategories = [x.strip() for subcategory in inc_df[inc_df['Year'].isin(years)]['Topic_Subcategories'].tolist() for x in subcategory.split(',')]
+    inc_subcategories = [x.strip() for subcategory in inc_df[inc_df['Year'].between(years[0], years[1])]['Topic_Subcategories'].tolist() for x in subcategory.split(',')]
     inc_subcategory_counts = pd.Series(inc_subcategories).value_counts()
     inc_subcategory_counts = inc_subcategory_counts.reindex(inc_subcategory_counts.nlargest(10).index)
 
@@ -82,11 +85,11 @@ if compare_parties:
 
     # Display the most common summary topics
     st.subheader("Most Common Summary Topics")
-    bjp_summary_topics = [x.strip() for topic in bjp_df[bjp_df['Year'].isin(years)]['Summary_Topics'].tolist() for x in topic.split(',')]
+    bjp_summary_topics = [x.strip() for topic in bjp_df[bjp_df['Year'].between(years[0], years[1])]['Summary_Topics'].tolist() for x in topic.split(',')]
     bjp_summary_topic_counts = pd.Series(bjp_summary_topics).value_counts()
     bjp_summary_topic_counts = bjp_summary_topic_counts.reindex(bjp_summary_topic_counts.nlargest(10).index)
 
-    inc_summary_topics = [x.strip() for topic in inc_df[inc_df['Year'].isin(years)]['Summary_Topics'].tolist() for x in topic.split(',')]
+    inc_summary_topics = [x.strip() for topic in inc_df[inc_df['Year'].between(years[0], years[1])]['Summary_Topics'].tolist() for x in topic.split(',')]
     inc_summary_topic_counts = pd.Series(inc_summary_topics).value_counts()
     inc_summary_topic_counts = inc_summary_topic_counts.reindex(inc_summary_topic_counts.nlargest(10).index)
 
@@ -100,11 +103,11 @@ if compare_parties:
 
     # Display the most common named entities
     st.subheader("Most Common Named Entities")
-    bjp_ner_list = [ner for ner_list in bjp_df[bjp_df['Year'].isin(years)]['NER'].tolist() for ner in eval(ner_list) if ner[0] not in stop_words + custom_stop_words]
+    bjp_ner_list = [ner for ner_list in bjp_df[bjp_df['Year'].between(years[0], years[1])]['NER'].tolist() for ner in eval(ner_list) if ner[0] not in stop_words + custom_stop_words]
     bjp_ner_counts = pd.Series([ner[0] for ner in bjp_ner_list]).value_counts()
     bjp_ner_counts = bjp_ner_counts.reindex(bjp_ner_counts.nlargest(10).index)
 
-    inc_ner_list = [ner for ner_list in inc_df[inc_df['Year'].isin(years)]['NER'].tolist() for ner in eval(ner_list) if ner[0] not in stop_words + custom_stop_words]
+    inc_ner_list = [ner for ner_list in inc_df[inc_df['Year'].between(years[0], years[1])]['NER'].tolist() for ner in eval(ner_list) if ner[0] not in stop_words + custom_stop_words]
     inc_ner_counts = pd.Series([ner[0] for ner in inc_ner_list]).value_counts()
     inc_ner_counts = inc_ner_counts.reindex(inc_ner_counts.nlargest(10).index)
 
@@ -125,7 +128,7 @@ else:
 
     # Display the most common topics as a radar chart
     st.subheader("Most Common Topics")
-    topics = [x.strip() for topic in df[df['Year'].isin(years)]['Domains'].tolist() for x in topic.split(',')]
+    topics = [x.strip() for topic in df[df['Year'].between(years[0], years[1])]['Domains'].tolist() for x in topic.split(',')]
     topic_counts = pd.Series(topics).value_counts()
     topic_counts = topic_counts.reindex(topic_counts.nlargest(10).index)
 
@@ -148,21 +151,21 @@ else:
 
     # Display the most common subcategories
     st.subheader("Most Common Subcategories")
-    subcategories = [x.strip() for subcategory in df[df['Year'].isin(years)]['Topic_Subcategories'].tolist() for x in subcategory.split(',')]
+    subcategories = [x.strip() for subcategory in df[df['Year'].between(years[0], years[1])]['Topic_Subcategories'].tolist() for x in subcategory.split(',')]
     subcategory_counts = pd.Series(subcategories).value_counts()
     subcategory_counts = subcategory_counts.reindex(subcategory_counts.nlargest(10).index)
     st.bar_chart(subcategory_counts)
 
     # Display the most common summary topics
     st.subheader("Most Common Summary Topics")
-    summary_topics = [x.strip() for topic in df[df['Year'].isin(years)]['Summary_Topics'].tolist() for x in topic.split(',')]
+    summary_topics = [x.strip() for topic in df[df['Year'].between(years[0], years[1])]['Summary_Topics'].tolist() for x in topic.split(',')]
     summary_topic_counts = pd.Series(summary_topics).value_counts()
     summary_topic_counts = summary_topic_counts.reindex(summary_topic_counts.nlargest(10).index)
     st.bar_chart(summary_topic_counts)
 
     # Display the most common named entities
     st.subheader("Most Common Named Entities")
-    ner_list = [ner for ner_list in df[df['Year'].isin(years)]['NER'].tolist() for ner in eval(ner_list) if ner[0] not in stop_words + custom_stop_words]
+    ner_list = [ner for ner_list in df[df['Year'].between(years[0], years[1])]['NER'].tolist() for ner in eval(ner_list) if ner[0] not in stop_words + custom_stop_words]
     ner_counts = pd.Series([ner[0] for ner in ner_list]).value_counts()
     ner_counts = ner_counts.reindex(ner_counts.nlargest(10).index)
     st.bar_chart(ner_counts)
